9d291f1527cad55eb2cbaadedcfe2300ab8ed17b
Mauricio Narvaez <nvz@meta.com>
2023-12-27 10:39:52 -0800

    Add engine.io-server-java as a submodule

--- mrpc-java/build.sh
+++ mrpc-java/build.sh
@@ -1,16 +1,11 @@
 # Ensure tools are set
 if [ -z ${JAVA_HOME+x} ]; then echo "JAVA_HOME is unset"; exit 1; fi
 
-ENGINEIO_PATH="$HOME/git/engine.io-server-java/"
+# Ensure all dependencies are ready
+git submodule update --init --recursive
 
-# Ensure dependencies exist
-if [ -d $ENGINEIO_PATH ]
-then
-    echo "Directory $ENGINEIO_PATH already exists!"
-else
-    echo "Directory $ENGINEIO_PATH doesn't exist, bailing!"
-    exit 1
-fi
+OWN_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+ENGINEIO_PATH="$OWN_PATH/../modules/engine.io-server-java/"
 
 # Ensure build tool exists
 if ! command -v mvn &> /dev/null
@@ -19,21 +14,19 @@ then
     exit 1
 fi
 
-OLD_PATH="$(pwd)"
-MRPC_JAVA_PATH="$HOME/git/mRPC/mrpc-java/"
-JAVA_CLI_PATH="$HOME/git/mRPC/samples/java-cli/"
+JAVA_CLI_PATH="$OWN_PATH/../samples/java-cli/"
 
 # First build engine.io
 cd $ENGINEIO_PATH
 mvn package -DskipTests
 
 # Copy into mrpc and sample
-mkdir -p $MRPC_JAVA_PATH/lib/libs/
+mkdir -p $OWN_PATH/lib/libs/
 mkdir -p $JAVA_CLI_PATH/app/libs/
-cp engine.io-server/target/engine.io-server-6.3.2.jar $MRPC_JAVA_PATH/lib/libs/engine.io-server-6.3.2.jar
+cp engine.io-server/target/engine.io-server-6.3.2.jar $OWN_PATH/lib/libs/engine.io-server-6.3.2.jar
 cp engine.io-server/target/engine.io-server-6.3.2.jar $JAVA_CLI_PATH/app/libs/engine.io-server-6.3.2.jar
 
 # Now build me and update sample
-cd $MRPC_JAVA_PATH
+cd $OWN_PATH
 ./gradlew jar
 cp lib/build/libs/lib.jar $JAVA_CLI_PATH/app/libs/mrpc-java.jar
